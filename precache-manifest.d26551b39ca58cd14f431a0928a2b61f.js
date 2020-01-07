78d8cfd7ea4b4830bd0d42cf599990ab083ba6b2
Ashraf Bawer <ashraf.bawer1@gmail.com>
2020-01-07 14:52:28 +0430

    Updates

---
+++ precache-manifest.d26551b39ca58cd14f431a0928a2b61f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad862c1fc59af9f2fb2d3d22d693cf2",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "c255c7c60e4188c07111",
+    "url": "/random-quote-machine/static/css/main.968db0cb.chunk.css"
+  },
+  {
+    "revision": "a64ffc381d40ae1a70d6",
+    "url": "/random-quote-machine/static/js/2.f2227b66.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/random-quote-machine/static/js/2.f2227b66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c255c7c60e4188c07111",
+    "url": "/random-quote-machine/static/js/main.5f68b71e.chunk.js"
+  },
+  {
+    "revision": "0adf6b55154200d7d3b4",
+    "url": "/random-quote-machine/static/js/runtime-main.9646a9dc.js"
+  }
+]);
